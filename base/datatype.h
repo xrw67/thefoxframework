92dc015abb28b1e45356cc8917d326898f33b74b
macwe <macwe@qq.com>
2013-10-10 17:40:37 +0800

    添加网络库的文件

--- base/datatype.h
+++ base/datatype.h
@@ -1,1 +1,19 @@
-typedef unsigned int uint;
+#ifndef _THEFOX_DATATYPE_H_
+#define _THEFOX_DATATYPE_H_
+
+namespace thefox
+{
+
+typedef INT64 int64_t
+
+// 字符串使用std::string 还是 thefox::String
+#ifdef THEFOX_STD_STRING
+	#include <string>
+	typedef std::string String;
+#else
+	#include <base/String.h>
+#endif
+
+}
+
+#endif // _THEFOX_DATATYPE_H_
