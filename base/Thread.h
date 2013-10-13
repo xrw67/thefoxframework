a8651796ee76abfe8ec072606b6eac859257c63e
macwe <macwe@qq.com>
2013-10-13 10:19:21 +0800

    重命名 tfthread.h 为 Thread.h 使用std::function重写线程类

---
+++ base/Thread.h
@@ -0,0 +1,51 @@
+#ifndef _THEFOX_THREAD_H_
+#define _THEFOX_THREAD_H_
+
+#include <functional>
+#include "afxmt.h"
+
+namespace TheFox
+{
+
+class Thread
+{
+public:
+    typedef std::function<void()> ThreadCallback;
+    
+	Thread(THreadCallback cb)
+        : _cb(cb)
+	{
+	}
+
+	void start()
+	{
+		_workStatus = true;
+		::AfxBeginThread(ThreadFunc, this);
+	}
+	
+	void stop()
+	{
+		_workStatus = false;
+		::WaitForSingleObject(_endEvent, INFINITE);
+	}
+    
+    bool getWorkStatus() const { return _workStatus; }
+priavte:
+    static UINT ThreadFunc(LPVOID p)
+	{
+		Thread *thread = (Thread *)p;
+		_cb();
+		thread->SetStopFlag();
+		return 0;
+	}
+	
+	void setStopFlag() { m_endEvent.SetEvent(); }
+
+    ThreadCallback _cb;
+	bool _workStatus;
+	CEvent _endEvent;
+};
+
+}; // namespace thefox
+
+#endif // _THEFOX_THREAD_H_
