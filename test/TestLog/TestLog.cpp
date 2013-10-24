7c8b811de74219fb32c4d4f1ca9bb2f69ca622f0
macwe <macwe@qq.com>
2013-10-24 22:32:31 +0800

    修改进程id输出格式

--- test/TestLog/TestLog.cpp
+++ test/TestLog/TestLog.cpp
@@ -6,13 +6,17 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	thefox::initLog("ffffff");
-
-	LOG_TRACE <<"dddddddd" << "dfdfd";
-	LOG_DEBUG << "dfdf" << 12 << -12.23;
-	LOG_INFO << "dfdf";
-	LOG_WARN << "dfdf";
-	LOG_ERROR << "dfdf";
+	thefox::initLog("TestLog");
+	int i=100;
+	while (--i)
+	{
+		Sleep(rand()%100);
+		LOG_TRACE <<"dddddddd" << "dfdfd";
+		LOG_DEBUG << "dfdf" << 12 << -12.23;
+		LOG_INFO << "dfdf";
+		LOG_WARN << "dfdf";
+		LOG_ERROR << "dfdf";
+	}
 
 	return 0;
 }
