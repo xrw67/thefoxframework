17207f6f9f5782542e0531d50f1cf531bc9727bc
macwe <macwe@qq.com>
2013-10-24 22:51:30 +0800

    添加日志库的使用说明

---
+++ log/README.md
@@ -0,0 +1,23 @@
+﻿日志操作库
+===============
+
+使用：
+#include <log/log.h>
+
+/*
+* @filename log.h
+* @brief log writer library
+* @author macwe@qq.com
+* example:
+* {
+*   // step1:
+*   thefox::initLog("basename");
+*   // step2:
+*   LOG_TRACE << "log text"<< 123 << ' ' << -3.14;
+*   LOG_DEBUG << "log text"<< 123 << ' ' << -3.14;
+*   LOG_INFO << "log text"<< 123 << ' ' << -3.14;
+*   LOG_WARN << "log text"<< 123 << ' ' << -3.14;
+*   LOG_ERROR << "log text"<< 123 << ' ' << -3.14;
+*   LOG_FATAL << "log text"<< 123 << ' ' << -3.14;
+* }
+*/
--- log/log.h
+++ log/log.h
@@ -1,3 +1,20 @@
+/*
+* @filename log.h
+* @brief log writer library
+* @author macwe@qq.com
+* example:
+* {
+*   // step1:
+*   thefox::initLog("basename");
+*   // step2:
+*   LOG_TRACE << "log text"<< 123 << ' ' << -3.14;
+*   LOG_DEBUG << "log text"<< 123 << ' ' << -3.14;
+*   LOG_INFO << "log text"<< 123 << ' ' << -3.14;
+*   LOG_WARN << "log text"<< 123 << ' ' << -3.14;
+*   LOG_ERROR << "log text"<< 123 << ' ' << -3.14;
+*   LOG_FATAL << "log text"<< 123 << ' ' << -3.14;
+* }
+*/
 #ifndef _THEFOX_LOG_H_
 #define _THEFOX_LOG_H_
 
@@ -20,6 +37,7 @@ void logFlushFunc()
 	g_logFile->flush();
 }
 
+/// @brief initialize log library
 void initLog(const String& basename)
 {
 	g_logFile.reset(new LogFile(basename, 200*1000));
