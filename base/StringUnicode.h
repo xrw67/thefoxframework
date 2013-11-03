6e77eecff26d7fdf9d2287f222d2a835951585a9
macwe <macwe@qq.com>
2013-11-03 21:57:13 +0800

    补充stringutil

---
+++ base/StringUnicode.h
@@ -0,0 +1,30 @@
+#ifndef _THEFOX_STRING_UNIDOCE_H_
+#define _THEFOX_STRING_UNIDOCE_H_
+
+namespace thefox
+{
+
+inline String bgkToUtf8(const String &str)
+{
+
+}
+
+inline String utf8ToGbk(const String &str)
+{
+
+}
+
+inline String gbkToUnicode(const String &str)
+{
+
+
+}
+
+inline String unicodeToGbk(const String &str)
+{
+	
+}
+
+}
+
+#endif // _THEFOX_STRING_UNIDOCE_H_
--- base/StringUtil.h
+++ base/StringUtil.h
@@ -1,15 +1,13 @@
 #ifndef _THEFOX_STRING_UTIL_H_
 #define _THEFOX_STRING_UTIL_H_
 
-#include <stdarg.h>
 #include <base/Types.h>
-
-#pragma warning(disable:4996)
 
 namespace thefox
 {
 
 static const int kMaxStringLen = 100 * 1024;
+
 inline int stoi(const String &str) { return (0 == str.length()) ? 0 : atoi(str.c_str()); }
 inline unsigned int stoui(const String &str) { return (0 == str.length()) ? 0 : static_cast<unsigned int>(atoi(str.c_str()));} 
 inline long stol(const String &str) { return (0 == str.length()) ? 0L : atol(str.c_str()); }
@@ -17,11 +15,32 @@ inline float stof(const String &str){ return (0 == str.length()) ? 0.0f : static
 inline double stod(const String &str) { return (0 == str.length()) ? 0.0 : atof(str.c_str()); }
 inline bool stob(const String &str) { return (0 == str.length() || str == "0" || str == "false" || str == "FALSE") ? false : true; }
 
-inline String toString(int val)
+inline String toString(const int val)
 {
 	char buf[32] = {0};
-	_snprintf(buf, sizeof(buf), "%d", val);
+	return _itoa(val, buf, 10);
+}
+
+inline String toString(const unsigned int val)
+{
+	char buf[32] = {0};
+	_snprintf(buf, sizeof(buf), "%u", val);
 	return buf;
+}
+inline String toString(const long val)
+{
+	char buf[32] = {0};
+	return _ltoa(val, buf, 10);
+}
+inline String toString(const double val)
+{
+	char buf[32] = {0};
+	_snprintf(buf, sizeof(buf), "%f", val);
+	return buf;
+}
+inline String toString(const bool val)
+{
+	return val ? "1" : "0";
 }
 
 inline String &format(String &str, const char *format, ...)
@@ -42,31 +61,77 @@ inline String &format(String &str, const char *format, ...)
 
 inline String trimLeft(const String &str)
 {
-
+	String::size_type index = str.find_first_not_of("\n\r\t");
+	if (index != String::npos)
+	{
+		str = str.substr(index);
+	}
+	return str;
 }
+
 inline String trimRight(const String &str)
 {
-
+	String::size_type index = str.find_last_not_of("\n\r\t");
+	if (index != String::npos)
+	{
+		str = str.substr(0, index + 1);
+	}
+	return str;
 }
 
 inline String trim(const String &str)
 {
-
+	return trimRight(trimLeft(str));
 }
 
 inline String toLower(const String &str)
 {
-
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 0x20;
+		}
+	}
 }
 
 inline String toUpper(const String &str)
 {
-
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 0x20;
+		}
+	}
 }
 
-inline String replace(const String &str, const String &from, const String &to, int start = 0)
+inline bool equalsIgnoreCase(const String &s1, const String &s2)
 {
+	if (s1.length() != s2.length())
+	{
+		return false;
+	}
+	for (int i = 0; i < s1.length(); ++i)
+	{
+		int diff = abs(s1[i] - s2[i]);
+		if (0 != diff || 0x20 != diff)
+		{
+			return false;
+		}
+	}
+	return true;
+}
 
+inline String replace(const String &str, const String &from, const String &to)
+{
+	String::size_type pos = 0;
+	while ((pos = str.find(from, pos)) != -1)
+	{
+		str.replace(pos, from.length(), to);
+		pos += to.length();
+	}
+	return str;
 }
 
 inline void swap(String &ls, String &rs)
