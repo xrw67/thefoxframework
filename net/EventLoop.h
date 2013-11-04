a21268f1b17539306bd9f148c8d73bfbd09439db
macwe@qq.com <macwe@qq.com>
2013-11-04 23:29:19 +0800

    modify eventloop

--- net/EventLoop.h
+++ net/EventLoop.h
@@ -2,29 +2,24 @@
 #define _THEFOX_NET_EVENTLOOP_H_
 
 #include <vector>
-
 #include <functionl>
+#include <memory>
+#include <base/Types.h>
 #include <base/noncopyable.hpp>
-
 #include <base/MutexLock.h>
 #include <base/Timestamp.h>
 #include <net/Callbacks.h>
 #include <net/TimerId.h>
 
-namespace muduo
-{
-namespace net
+namespace thefox
 {
 
 class Channel;
 class Poller;
 class TimerQueue;
 
-///
-/// Reactor, at most one per thread.
-///
-/// This is an interface class, so don't expose too much details.
-class EventLoop : boost::noncopyable
+/// @brief Reactor, at most one per thread.
+class EventLoop : noncopyable
 {
 public:
 	typedef boost::function<void()> Functor;
@@ -32,69 +27,48 @@ public:
 	EventLoop();
 	~EventLoop();  // force out-line dtor, for scoped_ptr members.
 
-	///
 	/// Loops forever.
-	///
 	/// Must be called in the same thread as creation of the object.
-	///
 	void loop();
 
 	/// Quits loop.
-	///
 	/// This is not 100% thread safe, if you call through a raw pointer,
 	/// better to call through shared_ptr<EventLoop> for 100% safety.
 	void quit();
-
-	///
+    
 	/// Time when poll returns, usually means data arrivial.
-	///
 	Timestamp pollReturnTime() const { return _pollReturnTime; }
 
-	int64_t iteration() const { return iteration_; }
+	int64_t iteration() const { return _iteration; }
 
 	/// Runs callback immediately in the loop thread.
 	/// It wakes up the loop, and run the cb.
 	/// If in the same loop thread, cb is run within the function.
 	/// Safe to call from other threads.
 	void runInLoop(const Functor& cb);
+    
 	/// Queues callback in the loop thread.
 	/// Runs after finish pooling.
 	/// Safe to call from other threads.
 	void queueInLoop(const Functor& cb);
 
-	#ifdef __GXX_EXPERIMENTAL_CXX0X__
-	void runInLoop(Functor&& cb);
-	void queueInLoop(Functor&& cb);
-	#endif
-
-	// timers
-
-	///
+	/// timers
 	/// Runs callback at 'time'.
 	/// Safe to call from other threads.
 	///
 	TimerId runAt(const Timestamp& time, const TimerCallback& cb);
-	///
+	
 	/// Runs callback after @c delay seconds.
 	/// Safe to call from other threads.
-	///
 	TimerId runAfter(double delay, const TimerCallback& cb);
-	///
+	
 	/// Runs callback every @c interval seconds.
 	/// Safe to call from other threads.
-	///
 	TimerId runEvery(double interval, const TimerCallback& cb);
-	///
+	
 	/// Cancels the timer.
 	/// Safe to call from other threads.
-	///
 	void cancel(TimerId timerId);
-
-	#ifdef __GXX_EXPERIMENTAL_CXX0X__
-	TimerId runAt(const Timestamp& time, TimerCallback&& cb);
-	TimerId runAfter(double delay, TimerCallback&& cb);
-	TimerId runEvery(double interval, TimerCallback&& cb);
-	#endif
 
 	// internal usage
 	void wakeup();
@@ -104,18 +78,18 @@ public:
 	// pid_t threadId() const { return threadId_; }
 	void assertInLoopThread()
 	{
-	if (!isInLoopThread())
-	{
-	abortNotInLoopThread();
+        if (!isInLoopThread())
+        {
+            abortNotInLoopThread();
+        }
 	}
-	}
-	bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
+	bool isInLoopThread() const { return _threadId == ::GetCurrentThreadId(); }
 	// bool callingPendingFunctors() const { return callingPendingFunctors_; }
-	bool eventHandling() const { return eventHandling_; }
+	bool eventHandling() const { return _eventHandling; }
 
 	static EventLoop* getEventLoopOfCurrentThread();
 
-	private:
+private:
 	void abortNotInLoopThread();
 	void handleRead();  // waked up
 	void doPendingFunctors();
@@ -124,23 +98,23 @@ public:
 
 	typedef std::vector<Channel*> ChannelList;
 
-	bool looping_; /* atomic */
-	bool quit_; /* atomic and shared between threads, okay on x86, I guess. */
-	bool eventHandling_; /* atomic */
-	bool callingPendingFunctors_; /* atomic */
-	int64_t iteration_;
-	const pid_t threadId_;
-	Timestamp pollReturnTime_;
-	boost::scoped_ptr<Poller> poller_;
-	boost::scoped_ptr<TimerQueue> timerQueue_;
-	int wakeupFd_;
+	bool _looping; /* atomic */
+	bool _quit; /* atomic and shared between threads, okay on x86, I guess. */
+	bool _eventHandling; /* atomic */
+	bool _callingPendingFunctors; /* atomic */
+	int64_t _iteration;
+	const uint32_t _threadId;
+	Timestamp _pollReturnTime;
+	std::unique_ptr<Poller> _poller;
+	std::unique_ptr<TimerQueue> _timerQueue;
+	int _wakeupFd;
 	// unlike in TimerQueue, which is an internal class,
 	// we don't expose Channel to client.
-	boost::scoped_ptr<Channel> wakeupChannel_;
-	ChannelList activeChannels_;
-	Channel* currentActiveChannel_;
-	MutexLock mutex_;
-	std::vector<Functor> pendingFunctors_; // @BuardedBy mutex_
+	std::unique_ptr<Channel> _wakeupChannel;
+	ChannelList _activeChannels;
+	Channel* _currentActiveChannel;
+	MutexLock _mutex;
+	std::vector<Functor> _pendingFunctors; // @BuardedBy mutex_
 };
 
 }
