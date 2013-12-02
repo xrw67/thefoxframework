027c73d77fe92f7f4d03ea6b2573e753698ec018
macwe <macwe@qq.com>
2013-12-02 21:24:52 +0800

    indeckmp fix bug

--- algorithm/string/patternmatching_kmp.h
+++ algorithm/string/patternmatching_kmp.h
@@ -6,7 +6,7 @@
 void createNextArray(const String &str, int *next)
 {
 	int i = 0;
-	int j = -1
+	int j = -1;
 	next[i] = j;
 	
 	while (str[i]) {
@@ -23,22 +23,27 @@ size_t indexKMP(const String &str, const String &patterns, int next[], size_t be
 	int i = beginPos;
 	int j = 0;
 	
-	while (str[i] || str[j]) {
-		if (str[i] == str[j]) {
+	while (i < str.length() && j < static_cast<int>(patterns.length())) {
+		if (-1 == j || str[i] == patterns[j]) {
 			++i;
 			++j;
 		} else {
 			j = next[j];
 		}
-		
 	}
-	return -1;
+
+	if (j >= patterns.length())
+		return i - j;
+	else
+		return -1;
 }
 
 size_t indexKMP(const String &str, const String &patterns, size_t beginPos = 0)
 {
 	size_t result = -1;
-	int *next = (int *)malloc(size(int) * str.length());
-	indexKMP(str, patterns, next, beginPos);
+	int *next = (int *)malloc(sizeof(int) * str.length());
+	createNextArray(patterns, next);
+	result = indexKMP(str, patterns, next, beginPos);
 	free(next);
+	return result;
 }
