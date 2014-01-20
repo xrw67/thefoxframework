b79670d3cc4f82017aaffb8717538c985b7159bf
macwe <macwe@qq.com>
2014-01-20 14:51:44 +0800

    commit breadboard class

---
+++ lab/Object.h
@@ -0,0 +1,17 @@
+﻿#ifndef _THEFOX_OBJECT_H_
+#define _THEFOX_OBJECT_H_
+
+namespace thefox
+{
+class Object
+{
+public:
+	void addRef() { ++_refCount; }
+	void decRef() { --_refCount; }
+	unsigned long getRefCount() const { return _refCount; }
+	
+private:
+	unsigned long _refCount; // 引用计数器
+};
+}
+#endif // _THEFOX_OBJECT_H_
