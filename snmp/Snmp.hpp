4de3a4a7c6d3f4cbe9606cdbdce46a65768ee7ee
macwe <macwe@qq.com>
2014-02-25 18:18:02 +0800

    modify snmp.hpp

--- snmp/Snmp.hpp
+++ snmp/Snmp.hpp
@@ -82,14 +82,16 @@ public:
 		{
 			if (SNMP_ERR_NOERROR == response->errstat)
 			{
+				values.clear();
 				for (vars = response->variables; vars; vars = vars->next_variable)
 				{
-					
+					values.push_back(asn2String(vars));
 				}
 			}
 			else
 			{
-				
+				// PDU´íÎó
+				return false;
 			}
 		}
 		else if (STAT_TIMEOUT == status)
@@ -112,7 +114,78 @@ public:
 	
 	bool getNext(const std::string &oids, std::string &newOids, std::vector<string> &values)
 	{
-		return false;
+		if (0 == oids.length())
+			return false;
+			
+		netsnmp_pdu *pdu = NULL;
+		netsnmp_pdu *response = NULL;
+		netsnmp_variable_list *vars = NULL;
+		
+		if (NULL == (pdu = snmp_pdu_create(SNMP_MSG_GET)))
+			return false;
+		
+		// ½âÎöOID
+		size_t pos = 0;
+		size_t lastPos = 0;
+		while (true)
+		{
+			pos = oids.find_first_of(',', lastPos);
+			if (std::string::npos == pos)
+			{
+				pos = oids.length();
+				if (pos != lastPos)
+				{
+					String oidBuf = oids.substr(lastPos, pos - lastPos);
+					snmp_add_null_var(pdu, oidBuf.c_str(), oidBuf.length());
+				}
+				break;
+			}
+			else
+			{
+				if (pos != lastPos)
+				{
+					String oidBuf = oids.substr(lastPos, pos - lastPos);
+					snmp_add_null_var(pdu, oidBuf.c_str(), oidBuf.length());
+				}
+			}
+			lastPos = pos + 1;
+		}
+		
+		int status = snmp_synch_response(_sessionPtr, pdu, response);
+		if (STAT_SUCCESS == status)
+		{
+			if (SNMP_ERR_NOERROR == response->errstat)
+			{
+				newOids = "";
+				values.clear();
+				for (vars = response->variables; vars; vars = vars->next_variable)
+				{
+					
+					values.push_back(asn2String(vars));
+				}
+			}
+			else
+			{
+				// PDU´íÎó
+				return false;
+			}
+		}
+		else if (STAT_TIMEOUT == status)
+		{
+			// ³¬Ê±
+			
+		}
+		else // status == STAT_ERROR
+		{
+			// Ê§°Ü
+			//TRACE(snmp_sess_perror(_sessionPtr));
+			
+		}
+		
+		if (response)
+			snmp_free_pdu(response);
+		
+		return (STAT_SUCCESS == status) ? true : false;
 	}
 	
 private:
@@ -140,7 +213,57 @@ private:
 		return true;
 	}
 	
-	const String variablesPrint(netsnmp_variable_list)
+	const String asn2String(netsnmp_variable_list *var)
+	{
+		
+		switch (var->type)
+		{
+			case ASN_INTEGER:
+			{
+				char str[32];
+				snprintf(str, sizeof(str), "%ld", *var->val.integer);
+				return str;
+			}
+			case ASN_OCTET_STR:
+			{
+				String str;
+				for(int i = 0; i < var->val_len; ++i)
+				{
+					str += var->val.string[i];
+				}
+				return str;
+			}
+			case ASN_BIT_STR:
+				
+			case ASN_OPAQUE:
+				
+			case ASN_OBJECT_ID:
+				
+			case ASN_TIMETICKS:
+				
+			case ASN_GAUGE:
+				
+			case ASN_COUNTER:
+				
+			case ASN_IPADDRESS:
+				
+			case ASN_NULL:
+			
+			case ASN_UINTEGER:
+				
+			case ASN_COUNTER64:
+			case ASN_OPAQUE_U64:
+			case ASN_OPAQUE_I64:
+			case ASN_OPAQUE_COUNTER64:
+			
+			case ASN_OPAQUE_FLOAT:
+				
+			case ASN_OPAQUE_DOUBLE:
+				
+			default:
+			
+		}
+	}
 	
 	struct snmp_session _session;
 	struct snmp_session *_sessionPtr;
