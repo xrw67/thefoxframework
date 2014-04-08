4d034cc187ae27d460d05ed81faf16754aa7f478
macwe <macwe@qq.com>
2014-04-08 20:17:11 +0800

    add xml.h

---
+++ base/Xml.h
@@ -0,0 +1,26 @@
+#ifndef _THEFOX_BASE_XML_H_
+#define _THEFOX_BASE_XML_H_
+
+#include <base/Types.h>
+#include <thirdparty/jsoncpp/json.h>
+#include <thirdparty/tinyxml/tinyxml.h>
+
+namespace thefox
+{
+
+bool xml2json()
+{
+	
+	
+	
+}
+
+bool json2xml()
+{
+	
+	
+}
+
+} // namespace thefox
+
+#endif // _THEFOX_BASE_XML_H_
