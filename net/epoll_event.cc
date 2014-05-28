4765b0341548f36c55c4d97e2bddff5d259a5285
macwe <macwe@qq.com>
2014-05-28 22:53:55 +0800

    add epoll to net library step 1

--- net/epoll_event.cc
+++ net/epoll_event.cc
@@ -1,1 +1,36 @@
-#include "EpollServer.h"
+#include "epoll_event.h"
+#include "tcp_connection.h"
+
+EpollEvent::EpollEvent()
+{}
+
+EpollEvent::~EpollEvent()
+{}
+
+bool EpollEvent::init()
+{}
+
+bool EpollEvent::addEvent(IoEvent *ev)
+{}
+
+bool EpollEvent::postClose(IoEvent *ev)
+{}
+
+void EpollEvent::addConnection()
+{}
+
+void EpollEvent::delConnection(TcpConnection *conn)
+{}
+
+bool EpollEvent::processEvents(uint32_t timer)
+{}
+
+bool EpollEvent::updateRead(IoEvent *ev)
+{}
+
+bool EpollEvent::updateWrite(IoEvent *ev)
+{}
+
+void EpollEvent::handler(IoEvent *ev)
+{}
+
--- net/epoll_event.h
+++ net/epoll_event.h
@@ -1,14 +1,33 @@
-//
-//  epoll.h
-//  
-//
-//  Created by macwe on 14-3-13.
-//
-//
+#ifndef _THEFOX_NET_EPOLL_H_
+#define _THEFOX_NET_EPOLL_H_
 
-#ifndef _epoll_h
-#define _epoll_h
+#include <base/types.h>
+#include <net/event.h>
+
+namespace thefox
+{
+	
+class TcpConnection;
+
+class EpollEvent
+{
+	EpollEvent();
+	~EpollEvent();
+
+	bool init();
+	bool addEvent(IoEvent *ev);
+	bool postClose(IoEvent *ev);
+	void addConnection();
+	bool delConnection(TcpConnection *conn);
+	bool processEvents(uint32_t timer);
+
+	bool updateRead(IoEvent *ev);
+	bool updateWrite(IoEvent *ev);
+private:
+	THEFOX_DISALLOW_EVIL_CONSTRUCTORS(EpollEvent);
+	void handler(IoEvent *ev);
 
 
-
-#endif
+};
+} // namespace thefox
+#endif // _THEFOX_NET_EPOLL_H_
